ee834c3a94f0bd13c4f0cfa5c6a388cde3314159
Joseph Alex Chakola <64978574+josephalx@users.noreply.github.com>
2024-02-15 14:13:04 -0500

    Init (#1)
    Co-authored-by: josephalx <litteouseph@gmail.com>

---
+++ infra/variables.tf
@@ -0,0 +1,27 @@
+variable "gcp_svc_key" {
+    
+}
+variable "gcp_project" {
+  
+}
+variable "gcp_region" {
+  
+}
+variable "gcp_vpc_name" {
+  
+}
+variable "subnet_name_1" {
+  
+}
+variable "subnet_1_cidr" {
+  
+}
+variable "subnet_name_2" {
+  
+}
+variable "subnet_2_cidr" {
+  
+}
+variable "subnet_1_custom_route" {
+  
+}
