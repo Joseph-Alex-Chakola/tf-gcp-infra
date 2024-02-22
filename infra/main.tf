a7661d868afa2701a185968e61144253661279d7
josephalx <litteouseph@gmail.com>
2024-02-22 16:23:14 -0500

    VM added

--- infra/main.tf
+++ infra/main.tf
@@ -23,13 +23,59 @@ resource "google_compute_subnetwork" "subnet2" {
     region = var.gcp_region
 }
 
-# Creating a route for subnet 1
+# Creating a route for VPC
 resource "google_compute_route" "webapp-route" {
     count = length(var.gcp_vpc)
     name = var.gcp_vpc[count.index].subnet_1_custom_route
     network = google_compute_network.VPC[count.index].id
     dest_range = "0.0.0.0/0"
     next_hop_gateway = "default-internet-gateway"
+}
+# Creating a firewall rule to allow inbound HTTP traffic
+resource "google_compute_firewall" "allow_http_inbound" {
+    count = length(var.gcp_vpc)
+    name = "${var.gcp_vpc[count.index].firewall_name}-inbound"
+    network = google_compute_network.VPC[count.index].id
+    allow {
+        protocol = "tcp"
+        ports = var.gcp_vpc[count.index].firewall_port
+    }
+    direction = "INGRESS"
+    source_ranges = var.gcp_vpc[count.index].firewall_source_range
+    target_tags = ["webapp-firewall"]
+}
+# Creating a firewall rule to deny inbound SSH traffic
+resource "google_compute_firewall" "deny_ssh_inbound" {
+    count = length(var.gcp_vpc)
+    name = "${var.gcp_vpc[count.index].firewall_name}-ssh-inbound"
+    network = google_compute_network.VPC[count.index].id
+    deny {
+        protocol = "tcp"
+        ports = ["22"]
+    }
+    direction = "INGRESS"
+    source_ranges = var.gcp_vpc[count.index].firewall_source_range
+}
+# Creating a Compute Engine instance with the VPC network
+resource "google_compute_instance" "webapp" {
+    count = length(var.gcp_vpc)
+    name = var.gcp_vpc[count.index].vm_name
+    machine_type = var.gcp_vpc[count.index].machine_type
+    zone = var.gcp_vpc[count.index].zone
+    boot_disk {
+        initialize_params {
+            image = var.gcp_vpc[count.index].image
+            size  = var.gcp_vpc[count.index].disk_size
+            type  = var.gcp_vpc[count.index].disk_type
+        }
+    }
+    network_interface {
+        network = google_compute_network.VPC[count.index].name
+        subnetwork = google_compute_subnetwork.subnet[count.index].id
+        access_config {
+        }
+    }
+    tags = ["webapp-firewall"]
 }
 
 
--- infra/variables.tf
+++ infra/variables.tf
@@ -17,6 +17,15 @@ variable "gcp_vpc" {
       subnet_name_2         = string
       subnet_2_cidr         = string
       subnet_1_custom_route = string
+      firewall_name         = string
+      firewall_port         = list(string)
+      firewall_source_range = list(string)
+      vm_name               = string
+      machine_type          = string
+      zone                  = string
+      image                 = string
+      disk_size             = number
+      disk_type             = string
     })
   )
 }
